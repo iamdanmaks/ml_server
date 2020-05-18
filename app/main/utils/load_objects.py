21083becfb8e21b98e03fdd0296fdcab4418fe0a
Daniil Maksymenko <konor00792@gmail.com>
2020-05-18 14:37:52 +0300

    Update load_objects.py

--- app/main/utils/load_objects.py
+++ app/main/utils/load_objects.py
@@ -5,7 +5,7 @@ from transformers import TFBertForTokenClassification
 
 def load(name):
     try:
-        with open(f'./app/main/models/utils/{name}_idx2tag.pickle') as handle:
+        with open(f'./app/main/models/utils/{name}_idx2tag.pickle', 'rb') as handle:
             idx2tag = pickle.load(handle)
     except FileNotFoundError:
         idx2tag = None
@@ -16,10 +16,10 @@ def load(name):
             num_labels=len(idx2tag) 
         )
     
-    with open(f'./app/main/models/weights/{name}.pickle') as handle:
+    with open(f'./app/main/models/weights/{name}.pickle', 'rb') as handle:
         model.set_weights(pickle.load(handle))
     
-    with open(f'./app/main/models/tokenizers/{name}.pickle') as handle:
+    with open(f'./app/main/models/tokenizers/{name}.pickle', 'rb') as handle:
         tokenizer = pickle.load(handle)
     
     return model, tokenizer, idx2tag
